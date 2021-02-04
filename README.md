539445a1b70a4089db524b77052a13c340984e4d
John XC Li <john_xc_li@wistron.com>
2021-02-04 15:53:04 +0800

    finish version

--- README.md
+++ README.md
@@ -1,18 +1,51 @@
 # 自动化截取刷卡记录
-## 指定日期，指定员工姓名或工号，截取刷卡记录。
+
+## Description
+指定日期，指定员工姓名或工号，截取刷卡记录。
+
+## 具体步骤
+1. 登陆网站
+[公司打卡记录查询](http://twhratsql.whq.wistron/OGWeb/LoginForm.aspx)    
+用户名<kbd>S0808001</kbd>&nbsp;&nbsp;密码 <kbd>S0808001</kbd>
+2. 获取登录后的cookie
+3. 打开查询页面，获取查询需要的字段
+4. 根据指定值查询打卡记录并将结果输出
+   1. 将查询结果整合保存
+   2. 判断状态
+5. 继续查询下一位
+6. 查询结束，保存成excel
+
 ## 判断状态
    * 正常
    * 异常
-     * 請假
-     * 遲到
-     * 早退
-     * 工時不足
-     * 只刷一次
+     * 請假 （迟到 + 工时不足）
+     * 遲到  上班打卡时间晚于08:50:59
+     * 早退  下班打卡时间早于16:50:00
+     * 工時不足 上班时间和下班时间之差小于9小时
+     * 只刷一次 默认认为是下班未打卡
+
+## 生成的Excel格式
+目前是每个人的记录放到一个sheet    
+代码中也提供了每个人生成一个excel的方法
+
+## How to use
+```
+git clone https://github.com/juanyilxc/kq.git
+node index.js
+```
+
+## Dependencies
+`"node-xlsx": "^0.16.1"`
+
+## 其他
+forked from [alex632/kq](https://github.com/alex632/)     
+
+Alex的v2.0 [alex632/kq](https://github.com/alex632/kq2)      
+花春榮v2.0 [s1001001/kq](https://github.com/s1001001/kq)    
+吴永辉的v2.0 [Yonghui1208/kq](https://github.com/Yonghui1208/kq)    
+王洪元的v2.0 [Hongyuan-001/kq](https://github.com/Hongyuan-001/kq)    
 
 
-# How to use
-git clone https://github.com/alex632/kq.git
-> node index.js
 
 # Insights
     Event driven & asynchronism are good. However they are somewhat not easy to comprehend.
--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 const http = require('http');   // NOTE: import default module
 const fs = require('fs');       // NOTE: import default module
 const querystring = require('querystring'); // NOTE: import default module
+const xlsx = require('node-xlsx')
 
 //
 // Step 1: Open login page to get cookie 'ASP.NET_SessionId' and hidden input '_ASPNetRecycleSession'.
@@ -24,7 +25,7 @@ function openLoginPage() {
         response.on('end', () => {
             let buff = Buffer.concat(chunks);
             let html = buff.toString();
-            if (response.statusCode===200) {
+            if (response.statusCode === 200) {
                 let fo = fs.createWriteStream('tmp/step1-LoginPage.html');
                 fo.write(html);
                 fo.end();
@@ -35,7 +36,7 @@ function openLoginPage() {
                     _ASPNET_SessionId = mc[1];
                     console.log(`Cookie ASP.NET_SessionId: ${_ASPNET_SessionId}`);
                 }
-                let patm =  new RegExp('<input type="hidden" name="_ASPNetRecycleSession" id="_ASPNetRecycleSession" value="(.*?)" />');
+                let patm = new RegExp('<input type="hidden" name="_ASPNetRecycleSession" id="_ASPNetRecycleSession" value="(.*?)" />');
                 let mm = patm.exec(html);
                 if (mm) {
                     _ASPNetRecycleSession = mm[1];
@@ -75,7 +76,7 @@ function login() {
         response.on('end', () => {
             let buff = Buffer.concat(chunks);
             let html = buff.toString();
-            if (response.statusCode===302) {
+            if (response.statusCode === 302) {
                 let fo = fs.createWriteStream('tmp/step2-login.html');
                 fo.write(html);
                 fo.end();
@@ -113,7 +114,7 @@ function login() {
         path: "/OGWeb/LoginForm.aspx",
         method: "POST",
         headers: {
-            'Cookie': 'ASP.NET_SessionId='+_ASPNET_SessionId,   // NOTED.
+            'Cookie': 'ASP.NET_SessionId=' + _ASPNET_SessionId,   // NOTED.
             'Content-Type': 'application/x-www-form-urlencoded',
             'Content-Length': Buffer.byteLength(postData)
         }
@@ -144,23 +145,23 @@ function step3() {
         response.on('end', () => {
             let buff = Buffer.concat(chunks);
             let html = buff.toString();
-            if (response.statusCode===200) {
+            if (response.statusCode === 200) {
                 let fo = fs.createWriteStream('tmp/step3.html');
                 fo.write(html);
                 fo.end();
-                let patm =  new RegExp('<input type="hidden" name="_ASPNetRecycleSession" id="_ASPNetRecycleSession" value="(.*?)" />');
+                let patm = new RegExp('<input type="hidden" name="_ASPNetRecycleSession" id="_ASPNetRecycleSession" value="(.*?)" />');
                 let mm = patm.exec(html);
                 if (mm) {
                     _ASPNetRecycleSession = mm[1];
                     console.log(`Element _ASPNetRecycleSession: ${_ASPNetRecycleSession}`);
                 }
-                let patv =  new RegExp('<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="(.*?)"');
+                let patv = new RegExp('<input type="hidden" name="__VIEWSTATE" id="__VIEWSTATE" value="(.*?)"');
                 let mv = patv.exec(html);
                 if (mv) {
                     __VIEWSTATE = mv[1];
                     console.log('Element __VIEWSTATE got');
                 }
-                let pate =  new RegExp('<input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="(.*?)"');
+                let pate = new RegExp('<input type="hidden" name="__EVENTVALIDATION" id="__EVENTVALIDATION" value="(.*?)"');
                 let me = pate.exec(html);
                 if (me) {
                     __EVENTVALIDATION = me[1];
@@ -192,6 +193,32 @@ function step3() {
     req.end();
 }
 
+//记录查询到了那天用于判断是否是工作日
+var begDate;
+//用来保存每个人的打开记录
+var results = [];
+//用来保存所有人的打卡记录
+var temp = [];
+
+//
+//将Step 4封装到Promise中
+//
+/**
+ * 参数定义和Step 4一样
+ */
+const pr = (beginDate, endDate, employeeIdOrName, nextPage) => {
+    begDate = endDate;
+    results = [];
+    return new Promise((resolve, reject) => {
+        inquire(beginDate, endDate, employeeIdOrName, nextPage, (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+            resolve(data);
+        });
+    });
+}
+
 //
 // Step 4: POST data to inquire.
 //
@@ -213,16 +240,19 @@ function inquire(beginDate, endDate, employeeIdOrName, nextPage, nextStep) {
         response.on('end', () => {
             let buff = Buffer.concat(chunks);
             let html = buff.toString();
-            if ( response.statusCode === 200 ) {
+            if (response.statusCode === 200) {
                 let result = parseKQ(html);
                 let fo = fs.createWriteStream(`tmp/step4-inquire-${employeeIdOrName}-${result.curPage}.html`);
                 fo.write(html);
                 fo.end();
-                if ( result.curPage < result.numPages ) {
+                if (result.curPage < result.numPages) {
                     inquire(beginDate, endDate, employeeIdOrName, true, nextStep);
                 } else {
                     console.log(`Inquiry about ${employeeIdOrName} is done.`);
-                    if ( nextStep ) {   // If provided.
+                    setState(results);
+                    temp.push(results);//将结果保存到temp
+                    //toSignelExcel(results);
+                    if (nextStep) {   // If provided.
                         nextStep();
                     }
                 }
@@ -251,7 +281,7 @@ function inquire(beginDate, endDate, employeeIdOrName, nextPage, nextStep) {
         'EndDateTB$_TimeEdit': endTime,
         'EmpNoNameCtrl1$InputTB': employeeIdOrName
     };
-    if ( nextPage ) {
+    if (nextPage) {
         postObj['GridPageNavigator1$NextBtn'] = 'Next Page';
     } else {
         postObj['QueryBtn'] = 'Inquire';
@@ -279,6 +309,7 @@ function inquire(beginDate, endDate, employeeIdOrName, nextPage, nextStep) {
     req.end(postData);
 }
 
+
 /**
  * Parse the input html to get 刷卡 data.
  * @param {*} html 
@@ -290,7 +321,7 @@ function parseKQ(html) {
     let numPages = 1;
     let rexTotal = new RegExp('<span id="GridPageNavigator1_CurrentPageLB">(.*?)</span>[^]*?<span id="GridPageNavigator1_TotalPageLB">(.*?)</span>');
     let match = rexTotal.exec(html);
-    if ( match ) {
+    if (match) {
         curPage = parseInt(match[1]);
         numPages = parseInt(match[2]);
         console.log(`Page: ${curPage} / ${numPages}`);
@@ -299,38 +330,200 @@ function parseKQ(html) {
     // Update __VIEWSTATE __EVENTVALIDATION
     let rexVS = new RegExp("__VIEWSTATE[\|](.*?)[\|]");
     let matVS = rexVS.exec(html);
-    if ( matVS ) {
+    if (matVS) {
         __VIEWSTATE = matVS[1];
     }
     let rexEV = new RegExp("__EVENTVALIDATION[\|](.*?)[\|]");
     let matEV = rexEV.exec(html);
-    if ( matEV ) {
+    if (matEV) {
         __EVENTVALIDATION = matEV[1];
     }
-
+    //倒计时 记录工作日
+    let workday = new Date(begDate);
     // Print 刷卡 data
     console.log(`/Department  /EID  /Name  /Clock Time`);
     while (true) {
-        let rex =  new RegExp('<td>(.*?)</td><td>&nbsp;</td><td><.*?>(.*?)</a></td><td>(.*?)</td><td>.*?</td><td>(.*?)</td>',
+        let rex = new RegExp('<td>(.*?)</td><td>&nbsp;</td><td><.*?>(.*?)</a></td><td>(.*?)</td><td>.*?</td><td>(.*?)</td>',
             'g');   // NOTE: 'g' is important
         let m = rex.exec(html);
         if (m) {
-            console.log(`${m[1]} ${m[2]} ${m[3]} ${m[4]}`);
+            console.log(`${m[1]} ${m[2]} ${m[3]} ${m[4]}`);//部门 工号 姓名 打卡日期时间
+            let datetime = new Date(m[4]);
+            //m的日期和计数日期比对，当计数大于m的日期，就判断计数日期是星期几，如果是周一~周五，表示当天未刷卡，加入一笔记录（后续会判断为请假，实际还可能是忘刷卡，放假，此处不考虑此种情况）
+            //result数据格式
+            //部门 工号 姓名 日期 第一次打卡时间 最后一次打卡时间 状态（请假 迟到等）
+            //while(workday.getMonth() != datetime.getMonth() && workday.getDate() != datetime.getDate()){
+            while (workday.toLocaleDateString("chinese",{month: '2-digit',day: '2-digit',year: 'numeric'}) > datetime.toLocaleDateString("chinese",{month: '2-digit',day: '2-digit',year: 'numeric'})) {
+                if (workday.getDay() > 0 && workday.getDay() < 6) {
+                    //results.push([m[1],m[2],m[3],workday,'','','']);
+                    results.push([m[1], m[2], m[3], new Date(workday.toLocaleDateString()), '', '', '']);
+                    //break;
+                }
+                workday.setDate(workday.getDate() - 1);
+            }
+
+            //let rex = new RegExp('([0-9]{1,2})\/([0-9]{1,2})\/[0-9]{4}\\s([0-9]{1,2}\:[0-9]{1,2}\:[0-9]{1,2})'); 
+            //let res = rex.exec(m[4]);//月份 日期 时间
+            //当result有数据时，判断此笔m是否和前一笔已经存在的数据是同一天
+            //如果为同一天，则放到result中的第一次打卡时间字段（因为数据是倒叙，所以第二次出现的为上班打卡时间）
+            if (results.length > 0) {
+                let lastdate = results[results.length - 1][3];
+
+                if (lastdate.getMonth() == datetime.getMonth() && lastdate.getDate() == datetime.getDate()) {
+                    results[results.length - 1][4] = datetime;
+                    workday.setDate(workday.getDate() + 1);//同一天的打卡记录，为了防止if判断外面把workday多减1天
+                }
+                else
+                    results.push([m[1], m[2], m[3], new Date(datetime.toLocaleDateString()), '', datetime, '']);
+            } else {
+                results.push([m[1], m[2], m[3], new Date(datetime.toLocaleDateString()), '', datetime, '']);
+            }
+            workday.setDate(workday.getDate() - 1);
+            //toExcel(m);
             html = html.substr(rex.lastIndex);
+
         } else {
-			break;
-		}
+            //
+            //workday.setDate(workday.getDate() - 1);
+            begDate = workday;
+            break;
+        }
     }
-    return {curPage: curPage, numPages: numPages};
+    return { curPage: curPage, numPages: numPages };
 }
 
-function askAll() {
-    inquire('2020-12-24', '2021-1-11', 'john', false,
-    ()=> inquire('2020-12-28', '2021-1-11', 'S2008001', false,
-    ()=> inquire('2020-12-24', '2021-1-6', 'ANNE', false,
-    ()=> inquire('2020-12-25', '2021-1-08', 'LEO MY CHEN', false,
-    ()=> inquire('2021-01-7', '2021-1-11', 'S0203002', false,
-    function() { console.log("All done.") } )))));
+//判断状态
+function setState(res) {
+    res.forEach(e => {
+        if (e[4] == '') {
+            e[6] += '请假';
+        } else {
+            if (e[5] == '')
+                e[6] += '下班未打卡';
+            //console.log(e[4]);
+            if (e[4] != '' && e[4].toLocaleTimeString('chinese', { hour12: false,hour:"2-digit" ,minute:"2-digit",second:"2-digit"}) > "08:50:59")
+                e[6] += '迟到';
+            if (e[5] != '' && e[5].toLocaleTimeString('chinese', { hour12: false,hour:"2-digit" ,minute:"2-digit",second:"2-digit"}) < "16:50:00")
+                e[6] += '早退';
+            if (e[4] != '' && e[5] != '' && (e[5].getTime() - e[4].getTime()) / (1000 * 60 * 60) < 9)
+                e[6] += '工时不足';
+            if (e[6] == '')
+                e[6] = '正常';
+        }
+    });
+    //迟到+工时不足=>请假
+    res.forEach(e => {
+        if (e[6].indexOf("工时不足") >= 0 && e[6].indexOf("迟到") >= 0)
+            e[6] = '请假';
+    })
+}
+
+//利用node xlsx导成excel
+function toExcel(mt) {
+    let datas = [];
+    let title = ['部门', '工号', '姓名', '日期', '上班时间', '下班时间', '状态']; //这是第一行 俗称列名 
+    //rows是个从数据库里面读出来的数组，大家就把他当成一个普通的数组就ok
+    mt.forEach( m => {
+        let data = []; // 其实最后就是把这个数组写入excel 
+        data.push(title); // 添加完列名 下面就是添加真正的内容了
+        m.forEach((e) => {
+            let arrInner = [];
+            arrInner.push(e[0]);
+            arrInner.push(e[1]);
+            arrInner.push(e[2]);
+            arrInner.push(e[3].toLocaleDateString());
+            arrInner.push(e[4] == '' ? '' : e[4].toLocaleTimeString('chinese', { hour12: false }));
+            arrInner.push(e[5] == '' ? '' : e[5].toLocaleTimeString('chinese', { hour12: false }));
+            arrInner.push(e[6]);
+            data.push(arrInner); //data中添加的要是数组，可以将对象的值分解添加进数组，例如：['1','name','上海']
+        });
+
+        let t = {
+            name: data[1][1],
+            data: data
+        }
+
+        datas.push(t);
+    });
+    writeXls(datas);
+};
+
+function writeXls(datas) {
+    let buffer = xlsx.build(datas);
+    fs.writeFileSync('the_content.xlsx', buffer, { 'flag': 'w' });//生成excel the_content是excel的名字，大家可以随意命名
+    
+    // fs.access('./the_content.xlsx',fs.constants.F_OK,err=>{
+    //     if(err)
+    //         fs.writeFileSync('./the_content.xlsx', buffer, { 'flag': 'w' });//生成excel the_content是excel的名字，大家可以随意命名
+    //     else
+    //         fs.appendFile('./the_content.xlsx', buffer, { 'flag': 'w' },(err) => {
+    //             if (err) throw err;
+    //             console.log('数据已被追加到文件');
+    //         })
+    // })
+           
+}
+
+//每个
+function toSignleExcel(m) {
+    let title = ['部门', '工号', '姓名', '日期', '上班时间', '下班时间', '状态']; //这是第一行 俗称列名 
+    //rows是个从数据库里面读出来的数组，大家就把他当成一个普通的数组就ok
+    let data = []; // 其实最后就是把这个数组写入excel 
+    data.push(title); // 添加完列名 下面就是添加真正的内容了
+    m.forEach((e) => {
+        let arrInner = [];
+        arrInner.push(e[0]);
+        arrInner.push(e[1]);
+        arrInner.push(e[2]);
+        arrInner.push(e[3].toLocaleDateString());
+        arrInner.push(e[4] == '' ? '' : e[4].toLocaleTimeString('chinese', { hour12: false }));
+        arrInner.push(e[5] == '' ? '' : e[5].toLocaleTimeString('chinese', { hour12: false }));
+        arrInner.push(e[6]);
+        data.push(arrInner); //data中添加的要是数组，可以将对象的值分解添加进数组，例如：['1','name','上海']
+    }); 
+    writesignleXls(data);
+};
+
+function writesignleXls(datas) {
+    let buffer = xlsx.build([
+        {
+            name: datas[1][1],
+            data: datas
+        }
+    ]);
+    fs.writeFileSync(`./result-${datas[1][1]}.xlsx`, buffer, { 'flag': 'w' });//生成excel the_content是excel的名字，大家可以随意命名
+           
+}
+
+// 回调版
+// function askAll() {
+//     inquire('2020-12-24', '2021-1-11', 'john', false,
+//     ()=> inquire('2020-12-28', '2021-1-11', 'S2008001', false,
+//     ()=> inquire('2020-12-24', '2021-1-6', 'ANNE', false,
+//     ()=> inquire('2020-12-25', '2021-1-08', 'LEO MY CHEN', false,
+//     ()=> inquire('2021-01-7', '2021-1-11', 'S0203002', false,
+//     function() { console.log("All done.") } )))));
+// }
+
+// let promise = new Promise((resolve, reject) => {
+//     // 初始化promise状态为padding
+//     // 执行异步操作
+//     if(异步操作成功){
+//         resolve(value) // 修改promise的状态为fullfiled
+//     } else {
+//         reject(errMsg) // 修改promise的状态为rejected
+//     }
+// })
+
+//Promise版本
+async function askAll() {
+    await pr('2020-12-24', '2021-1-14', 'john', false)
+    await pr('2020-12-28', '2021-1-11', 'S2008001', false)
+    // await pr('2020-12-24', '2021-1-6', 'ANNE', false)
+    // await pr('2020-12-25', '2021-1-08', 'LEO MY CHEN', false)
+    // await pr('2021-01-7', '2021-1-11', 'S0203002', false)
+    toExcel(temp)
+    console.log("All done.")
 }
 
 openLoginPage();    // Where it all begins.
